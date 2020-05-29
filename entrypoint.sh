5ae34558d43884a1946318f4159647e3ef17ddb8
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2020-05-29 14:38:07 -0300

    MVP para testes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
 
-EXIT_IF_AN_ERROR_OCCURRED() {
-    LAST_CODE=$?;
-    echo "$LAST_CODE";
-    if [[ $LAST_CODE != 0 ]];
-    then
-        exit $LAST_CODE;
-    fi
-}
-
-TEST() {
-    cd build
-    ./tests
-    EXIT_IF_AN_ERROR_OCCURRED;
-    cd ..
-}
-
-if [[ $1 == "test" ]];
-then
-    TEST;
-else
-    echo "Only supports test"
-fi
+# Entrypoint is the simulator itself!
+./build/vss-simulator | tee log.txt
